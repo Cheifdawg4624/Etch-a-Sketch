cf848e0abbe13fe3cd8b4b5c20e142865e955df7
Cheifdawg4624 <rmaher4624@gmail.com>
2023-04-12 21:24:39 -0400

    added ability to create board into ANY amount of squares in the same area

--- eas.js
+++ eas.js
@@ -19,3 +19,35 @@ document.querySelectorAll(".square").forEach((square) => {
 function changeColor(e) {
   e.target.classList.add("hovered");
 }
+
+// Button to create board
+const button = document.querySelector("button");
+
+button.addEventListener("click", newBoard);
+
+// New board Function
+function newBoard() {
+  let boardSize = prompt("How big of a board?(How many squares)");
+  if (boardSize > 100) {
+    boardSize = prompt("Please pick a number smaller than one hundred");
+    parseFloat(boardSize);
+  } else {
+    parseFloat(boardSize);
+  }
+  const selectContainer = document.getElementsByClassName("container");
+  while (selectContainer[0].firstChild) {
+    selectContainer[0].removeChild(selectContainer[0].firstChild);
+  }
+  const boardSizeSquared = boardSize * boardSize;
+  for (i = 0; i < boardSizeSquared; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    document.querySelector(".container").appendChild(square);
+  }
+  const boardSizeWidth = (1 / boardSize) * 100;
+  document.querySelectorAll(".square").forEach((e) => {
+    e.style.width = `${boardSizeWidth}%`;
+    e.style.height = `${boardSizeWidth}%`;
+    e.addEventListener("mouseover", changeColor);
+  });
+}
